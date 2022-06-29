06e10ec503b3df873eb24a477532c4b70c7ddcf1
Im Baba <108435751+BabaLabs@users.noreply.github.com>
2022-06-29 18:44:17 -0400

    Update Calendar Delegates v2.ps1

--- Calendar Delegates v2.ps1
+++ Calendar Delegates v2.ps1
@@ -4,29 +4,29 @@ $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown'); #I put this in here bec
 Connect-ExchangeOnline -UserPrincipalName $AdminAccount #Connects the user to Exchange Online using the email address provided. They will get a pop-up prompt for password and MFA where applicable.
 
 do {
-    $TargetCalendar = Read-Host "Please enter the email address of the calendar that is BEING SHARED"` #Prompts the user for the email address being shared
+    $TargetMailbox = Read-Host "Please enter the email address of the mailbox that is BEING SHARED"` #Prompts the user for the email address being shared
 
     Write-Host 'Press any key to continue...'; `
         $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown'); #The prompts for Press any key are put in just for user confirmation and ability to close script if no longer necessary
 
-    $DelegateAccount = Read-Host "Please enter the email address of the user GETTING ACCESS to the calendar"` #Prompts the user for the email address that is getting access
+    $DelegateAccount = Read-Host "Please enter the email address of the user GETTING ACCESS to the mailbox"` #Prompts the user for the email address that is getting access
 
     Write-Host 'Press any key to continue...'; `
         $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown'); #Another confirmation prompt, and opportunity to easily close the script
 
-    Add-MailboxFolderPermission -Identity ${TargetCalendar}:\Calendar -User $DelegateAccount -AccessRights Editor -SharingPermissionFlags Delegate #Runs the following command based on the input provided by the user.
+    Add-MailboxPermission -Identity $TargetMailbox -User $DelegateAccount -AccessRights FullAccess -InheritanceType All -Automapping $False #Runs the following command based on the input provided by the user.
 
     Write-Host 'Press any key to continue...'; `
         $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown'); #Another confirmation prompt, and opportunity to easily close the script
 
-    Write-Output 'Access to the calendar of '${TargetCalendar}' has been given to '${DelegateAccount}''`} #Visual confirmation for the user that the permissions have been added.
+    Write-Output 'Outlook Auto-Mapping for mailbox '${TargetMailbox}' has been disabled for '${DelegateAccount}''`} #Visual confirmation for the user that the permissions have been added.
 
     function Show-Menu { #The following creates a visual menu showing the user options to repeat the commands for more/different delegates
         param (
             [string]$Title = 'Confirmation Menu'
         )
         Clear-Host
-        Write-Host "================ $Title ================" 
+        Write-Host "================ $Title ================"
 
         Write-Host "Do you need to add additional delegates?"
         Write-Host "1: Press '1' to add more delegates."
